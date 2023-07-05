ada527f35a848d7e1edb2e69f5e82ea98aeff474
abhinavsinha23 <abhinavsinha36@hotmail.com>
2023-07-05 17:49:02 +0100

    loopActivity7

---
+++ loopActivity7.js
@@ -0,0 +1,18 @@
+console.log("Counting from 0 to 9 with a for loop:")
+for (i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log("With a while loop:")
+let j = 0;
+while (j < 10){
+    console.log(j);
+    j++;
+}
+
+console.log("With a do...while loop:")
+let k = 0;
+do {
+    console.log(k);
+    k++;
+} while (k < 10);

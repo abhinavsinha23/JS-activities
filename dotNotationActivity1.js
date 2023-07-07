dd6ce136a48e289fd16aa51563f55def588fe6f3
abhinavsinha23 <abhinavsinha36@hotmail.com>
2023-07-07 17:14:33 +0100

    dotnotationactivity

---
+++ dotNotationActivity1.js
@@ -0,0 +1,11 @@
+console.log((Math.random()*10).toFixed(2))
+for (var a = 0; a < 2; a++){
+    for (var i = 0; i < 3; i++) {
+        console.log("   |   |   ")
+    }
+    console.log("-----------")
+}
+
+for (var i = 0; i < 3; i++) {
+    console.log("   |   |   ")
+}
